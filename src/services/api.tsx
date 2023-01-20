94b60c87ec38fb92970ed2f5402e7594072494ac
MatthewEbert <mateusz@ebert.app>
2023-01-20 16:13:29 +0100

    Update

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import TokenService from "./token.service";
 import { User } from "./userProps";
 import { Network } from "@capacitor/network";
-import { Storage } from "@capacitor/storage";
+import { Preferences } from "@capacitor/preferences";
 
 const instance = axios.create({
   // timeout: 15000,
@@ -53,10 +53,10 @@ instance.interceptors.response.use(
           //   window.location.replace("/login");
           // }
 
-          const { value } = await Storage.get({ key: "OfflineRequests" });
-          await Storage.clear();
+          const { value } = await Preferences.get({ key: "OfflineRequests" });
+          await Preferences.clear();
           if (value) {
-            await Storage.set({
+            await Preferences.set({
               key: "OfflineRequests",
               value: value,
             });
